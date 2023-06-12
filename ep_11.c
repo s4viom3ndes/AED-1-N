7f62e8cf838c9c81102fbe89d28f3a271d9028f4
Savio Mendes <103843018+s4viom3ndes@users.noreply.github.com>
2023-06-12 11:29:13 -0300

    Update ep_11.c

--- ep_11.c
+++ ep_11.c
@@ -1,131 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int funcaoHash(int chave, int TAM){
+int funcaoHash(int chave, int TAM) {
   return chave % TAM;
 }
 
-typedef struct no{
+typedef struct no {
   int chave;
-  struct no *prox;
-}No;
+  struct no* prox;
+} No;
 
-typedef struct Lista{
+typedef struct Lista {
   No* inicio;
   int tam;
-}Lista;
+} Lista;
 
-void inciaLista(Lista *l){
+void iniciaLista(Lista* l) {
   l->inicio = NULL;
   l->tam = 0;
 }
 
-/*
-entradas: 
-2
-13 9
-44 45 49 70 27 73 92 97 95
-7 8
-35 12 2 17 19 51 88 86
-*/
-
-void inserelista(Lista *l, int valor){
-No *novoNo = (No*) malloc(sizeof(No));
+void inserirLista(Lista* l, int valor) {
+  No* novoNo = (No*)malloc(sizeof(No));
   novoNo->chave = valor;
   novoNo->prox = NULL;
 
-  // Verifica se a lista está vazia
   if (l->inicio == NULL) {
     l->inicio = novoNo;
   } else {
-    
-    No *atual = l->inicio;
+    No* atual = l->inicio;
     while (atual->prox != NULL) {
       atual = atual->prox;
     }
-   
     atual->prox = novoNo;
   }
 
- 
   l->tam++;
 }
 
-int buscarNaLista(Lista *l, int chave){
-
+void imprimirLista(Lista* l) {
   No* aux = l->inicio;
-
-  while(aux != NULL && aux->chave != chave)
-    aux = aux->prox;
-  if(aux){
-    return aux->chave;
-  }
-  return 0;
-}
-
-void imprimeLista(Lista *l){
-  No *aux = l->inicio;
- 
-  while(aux){
-    printf(" ");
-    printf("%d ->", aux->chave);
+  while (aux != NULL) {
+    printf("%d -> ", aux->chave);
     aux = aux->prox;
   }
+  printf("\\\n");
 }
-//----------------------------------------------------------------
-void iniciaTabelaLista(Lista t[], int TAM){
+
+void iniciaTabelaLista(Lista t[], int TAM) {
   int i;
-  for(i=0; i<TAM; i++){
-   inciaLista(&t[i]);//empty position
+  for (i = 0; i < TAM; i++) {
+    iniciaLista(&t[i]); // posição vazia
   }
 }
 
-void inserirLista(Lista t[], int valor, int TAM){
-  int id = funcaoHash(valor, TAM);
-  inserelista(&t[id], valor);
+void inserirElementos(Lista t[], int TAM, int M) {
+  int valor, i;
+  for (i = 0; i < M; i++) {
+    scanf("%d", &valor);
+    int id = funcaoHash(valor, TAM);
+    inserirLista(&t[id], valor);
+  }
 }
 
-
-
-void imprimirLista(Lista t[], int TAM){
+void imprimirTabela(Lista t[], int TAM) {
   int i;
-  for(i = 0; i<TAM; i++){
-    printf("%d ->", i);
-    imprimeLista(&t[i]);
-      printf(" \\");
-    printf("\n");
-  }  
-  printf("\n");
+  for (i = 0; i < TAM; i++) {
+    printf("%d -> ", i);
+    imprimirLista(&t[i]);
+  }
 }
 
+int main() {
+  int testes, C, M, i;
 
-int main(void) {
-  int C, M, testes, retorno, valor, i=0;
-  
   scanf("%d", &testes);
-  
- 
-  
-  while(i < testes){
-    scanf("%d %d", &C, &M);
-    
-    Lista tabela[C];
-    
-    iniciaTabelaLista(tabela, C);
-    
-   
-    
-    for(int j = 0; j < M; j++){
-      scanf("%d", &valor);
-        inserirLista(tabela, valor, C);
-    }
-    imprimirLista(tabela, C);
 
-      
-    i++;
+  for (i = 0; i < testes; i++) {
+    scanf("%d %d", &C, &M);
+
+    Lista tabela[C];
+    iniciaTabelaLista(tabela, C);
+    inserirElementos(tabela, C, M);
+    imprimirTabela(tabela, C);
+
+    if (i < testes - 1) {
+      printf("\n");
+    }
   }
 
- 
   return 0;
 }
